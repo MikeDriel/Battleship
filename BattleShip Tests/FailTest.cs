a2abaa315be31695d579bba903ff22f41cd6b04d
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2023-04-20 09:49:20 +0200

    Create FailTest.cs

---
+++ BattleShip Tests/FailTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace BattleShip_Tests
+{
+    public class FailTest
+    {
+
+        [Fact]
+        public void Test_Fail()
+        {
+
+            // Assert
+            Assert.Fail("fail");
+
+        }
+    }
+}
